f16d7836ad93d1ce93e8f9740f4ed98b733457e0
sanket <stupe@uptycs.com>
2021-07-21 03:01:52 +0530

    color lualine changed

--- lua/sanket/lualine.lua
+++ lua/sanket/lualine.lua
@@ -3,6 +3,9 @@ local custom_ayu = require'lualine.themes.ayu_dark'
 --custom_ayu.normal.a.fg = '#ffffff' -- rgb colors are supported
 custom_ayu.normal.c.fg = '#ffffff' -- rgb colors are supported
 custom_ayu.normal.a.fg = '#ffffff' -- rgb colors are supported
+custom_ayu.insert.a.fg = '#ffffff' -- rgb colors are supported
+custom_ayu.visual.a.fg = '#ffffff' -- rgb colors are supported
+custom_ayu.visual.a.bg = '#ff9f00' -- rgb colors are supported
 require'lualine'.setup{
   options={
     theme=custom_ayu
